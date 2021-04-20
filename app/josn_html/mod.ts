b37c73a91c80bdbe7cf224ad460aac58f357cbd8
Totoro-moroku <shinsi4545@gmail.com>
2021-04-20 22:47:17 +0900

    test

--- app/josn_html/mod.ts
+++ app/josn_html/mod.ts
@@ -1,3 +1,5 @@
+import{} App from '../app'
+
 function handleRequest(request) {
   const { pathname } = new URL(request.url);
 
@@ -32,19 +34,7 @@ function handleRequest(request) {
   }
 
   return new Response(
-    `<body
-      align="center"
-      style="font-family: Avenir, Helvetica, Arial, sans-serif; font-size: 1.5rem;"
-    >
-    <div>aaaa</div>
-      <h1>Return JSON and/or HTML Example</h1>
-      <p>
-        <a href="/html">/html</a> - responds with HTML to the request.
-      </p>
-      <p>
-        <a href="/json">/json</a> - responds with JSON to the request.
-      </p>
-    </body>`,
+    App,
     {
       headers: {
         "content-type": "text/html; charset=UTF-8",
